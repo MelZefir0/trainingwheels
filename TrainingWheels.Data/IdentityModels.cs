32c3b6535309a573fb82d42f904ef8a2a5f16855
Liz <wanderlust3x3@gmail.com>
2018-01-16 23:44:23 -0500

    inital

---
+++ TrainingWheels.Data/IdentityModels.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System;
+
+namespace TrainingWheels.Data
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string Note { get; set; }
+
+        [Required]
+        public int HnWScore { get; set; }
+
+        [Required]
+        public int HygScore { get; set; }
+
+        [Required]
+        public int FinScore { get; set; }
+
+        [Required]
+        public int SocScore { get; set; }
+
+        [Required]
+        public int CnOScore { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<ActivityEntity> Activities { get; set; }
+        public DbSet<ArchiveEntity> Archives { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    public class SecurityRoles
+    {
+        public const string Admin = "admin";
+    }
+}
